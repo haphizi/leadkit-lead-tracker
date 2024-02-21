ca1364a12c3b1679bcf16a6d65978d4819c60b6d
haphizi <61571184+haphizi@users.noreply.github.com>
2024-02-21 13:34:30 +0800

    feat: added input el, button el, saving and displaying leads list functionality

---
+++ index.js
@@ -0,0 +1,34 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+
+const inputBtn = document.getElementById("input-btn");
+
+const ulEl = document.getElementById("ul-el");
+
+// better separation of concerns, doesnt use onclick method
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  renderLeads();
+  inputEl.value = ""
+});
+
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+    
+    listItems += "<li>" + myLeads[i] + "</li>";
+
+    //method .1
+    //   ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+
+    // method .2 to createElement and append...
+    //   const li = document.createElement("li")
+    //   li.textContent = myLeads[i]
+    //   ulEl.append(li)
+  }
+
+  // 3. Render the listItems inside the unordered list using ulEl.innerHTML
+  ulEl.innerHTML = listItems;
+}
